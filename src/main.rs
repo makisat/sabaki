04f30d8ab6cbad279dcc1b57ed6a12f18e8614de
makisat <makiya28sato@gmail.com>
2024-02-02 02:16:27 -0500

    sort videos

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{fs, io::ErrorKind};
 fn main() {
 
     let image_ext: Vec<&str> = vec!["png", "jpg", "jpeg", "gif"];
+    let video_ext: Vec<&str> = vec!["mp4", "mov", "avi"];
 
     let paths = fs::read_dir("./").unwrap();
 
@@ -16,22 +17,31 @@ fn main() {
             let ext: Vec<&str> = path_name.split(".").collect();
 
             if ext.len() > 1 {
-                println!("ext: {}", ext[1]);
-                let ext_name = ext[1];
+                let ext_name = ext[ext.len() - 1];
+
                 if image_ext.contains(&ext_name) {
-                    let res = fs::rename(path_name, "./images/".to_owned() + path_name);
-                    if let Err(err) = res {
-                        match err.kind() {
-                            ErrorKind::NotFound => {
-                                fs::create_dir("./images").unwrap();
-                                fs::rename(path_name, "./images/".to_owned() + path_name).expect(format!("error occured during moving {}", path_name).as_str());
-                            },
-                            _ => panic!("error occured during moving the files")
-                        }
-                    }
+                    sort_ext(&path_name, "images");
+                }
+
+                if video_ext.contains(&ext_name) {
+                    sort_ext(&path_name, "video");
                 }
             }
         }
     }
+}
+
+fn sort_ext(path_name: &&str, dst: &str) {
+    let res = fs::rename(path_name, format!("./{}/{}", dst, path_name));
+    if let Err(err) = res {
+        match err.kind() {
+            ErrorKind::NotFound => {
+                fs::create_dir("./".to_owned() + dst).unwrap();
+                fs::rename(path_name, format!("./{}/{}", dst, path_name)).expect(format!("error occured during moving {}", path_name).as_str());
+            },
+            _ => panic!("error occured during moving the files")
+        }
+    }
+
 }
 
